339f9980089686790c9a6f54b55d9572946f2920
Eduardo Silva de Brito <113521546+Eduardosbrito@users.noreply.github.com>
2023-12-24 14:54:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,10 @@ function obterConselho() {
   fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
-      const conselhoEmIngles = data.slip.advice;
-      traduzirParaPortugues(conselhoEmIngles);
+      document.getElementById('result').innerHTML = "Conselho: " + data.slip.advice;
     })
     .catch(error => {
       document.getElementById('result').innerHTML = "Erro ao obter conselho.";
-    });
-}
-
-function traduzirParaPortugues(textoEmIngles) {
-  const apiKey = 'SUA_CHAVE_DE_API'; // Substitua com sua chave de API do Yandex Translate
-  const endpoint = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${encodeURIComponent(textoEmIngles)}&lang=en-pt`;
-
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-      const conselhoTraduzido = data.text[0];
-      document.getElementById('result').innerHTML = "Conselho: " + conselhoTraduzido;
-    })
-    .catch(error => {
-      document.getElementById('result').innerHTML = "Erro ao traduzir conselho.";
     });
 }
 
